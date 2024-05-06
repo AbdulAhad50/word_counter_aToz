5adb61fa08d4caacb419591d801b43669c86e9b5
Abdul Ahad <156930361+AbdulAhad50@users.noreply.github.com>
2024-05-06 05:54:04 -0700

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+console.log("=".repeat(50));
+console.log(chalk.bold.cyanBright("\t \t Word Counter App"));
+
+console.log("=".repeat(50),"\n");
+
+let answer = await inquirer.prompt([
+    {
+        name : "sentence",
+        type : chalk.red("input"),
+        message : chalk.bold.cyanBright("Enter a Sentence:")
+    }
+])
+
+let words = answer.sentence.trim().split(" ");
+
+
+console.log(chalk.bold("\n- sentence word:"));
+console.log(words);
+console.log(chalk.bold(`\n-Total words count is: ${chalk.bold.redBright(words.length)}`));
+console.log("=".repeat(50));
+
+
+
+
+
